9ba63499f39de5df4b0efbe0b0c23e90e8b1c284
Odilxon <mmuhtor@gmail.com>
2021-10-04 10:19:37 +0500

    first

---
+++ app.py
@@ -0,0 +1,15 @@
+import telebot
+TOKEN = "1258084373:AAFnxvjZ67Gx4iMROrCtyk21h981G-4xJWE"
+bot = telebot.TeleBot(TOKEN, parse_mode=None)
+
+
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    print(message)
+    bot.reply_to(message, "Are you horny right now?")
+    with open("horny.mp4", "rb") as f:
+        bot.send_video(message.chat.id,f)
+
+bot.infinity_polling()

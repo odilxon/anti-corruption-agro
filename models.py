5a4a521ac5db2c16e04dee9d53ce8df0ecfda20f
jurody <kamilov.2000@mail.ru>
2021-10-08 10:56:57 +0500

    import or

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import func, and_, delete, create_engine, text, MetaData, Integer, String, Column, ForeignKey, Date, Time, DateTime, Boolean, extract
+from sqlalchemy import func, and_, or_, delete, create_engine, text, MetaData, Integer, String, Column, ForeignKey, Date, Time, DateTime, Boolean, extract
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session, subqueryload, joinedload, relationship
 from sqlalchemy.sql.expression import null

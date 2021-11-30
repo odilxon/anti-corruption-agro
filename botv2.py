4457c2035e328fd674329995f2b80530c619ed1a
jurody <kamilov.2000@mail.ru>
2021-11-30 18:55:25 +0500

    fix

--- botv2.py
+++ botv2.py
@@ -225,16 +225,17 @@ def message_reply(message):
                         key = item['key'],
                         value = item['value']
                     )
+                
                 session.add(com_d)
                 session.commit()
             
             
             bot.send_message(message.chat.id,"Сз жонаткан нарсалар кошилди! стартни босинг /start",reply_markup=None)
-
+            user_data[message.chat.id] = []
             ss.step = "test"
             session.add(ss)
             session.commit()
-        
+            return 1
         text = message.text
         user_data[message.chat.id].append({
             "key" : "text",
